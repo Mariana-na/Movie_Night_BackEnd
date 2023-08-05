9ba6cb3120497e7c82aa6a6f84daf35ea499b901
Hamed Jalili <itjalili82@gmail.com>
2023-08-05 14:12:02 +0200

    event.model update

--- models/Event.model.js
+++ models/Event.model.js
@@ -1,1 +1,26 @@
-<h1>Here is Event Model Page</h1>
+const { Schema, model } = require("mongoose");
+
+const eventSchema = new Schema(
+  {
+    movie: {
+      type: String,
+      required: [true, 'Movie name is required.'],
+    },
+    snack: {
+      type: String,
+      required: [true, 'Snack name is required.'],
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'User ID is required.'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Event = model("Event", eventSchema);
+
+module.exports = Event;
