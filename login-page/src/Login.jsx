fd705789771f2ac63f17f79eb5b6386badf2c9ed
Hamed Jalili <itjalili82@gmail.com>
2023-08-05 11:53:03 +0200

    install react, create files and folders, add codes

---
+++ login-page/src/Login.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here (e.g., send data to the backend for authentication)
+    console.log("Email:", email);
+    console.log("Password:", password);
+    // You can replace the console.log with an actual login API call here.
+  };
+
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
