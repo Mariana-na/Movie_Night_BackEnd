bcedad6c56a251e9fc89ff316c1c4c8723a18e28
Hamed Jalili <itjalili82@gmail.com>
2023-08-05 14:18:11 +0200

    event.model update

--- models/Event.model.js
+++ models/Event.model.js
@@ -2,19 +2,36 @@ const { Schema, model } = require("mongoose");
 
 const eventSchema = new Schema(
   {
-    movie: {
-      type: String,
-      required: [true, 'Movie name is required.'],
-    },
-    snack: {
-      type: String,
-      required: [true, 'Snack name is required.'],
-    },
-    userId: {
+    user: {
       type: Schema.Types.ObjectId,
       ref: 'User',
       required: [true, 'User ID is required.'],
     },
+    dateTime: {
+      type: Date,
+      required: [true, 'Date and time are required.'],
+    },
+    location: {
+      type: String,
+      required: [true, 'Location is required.'],
+    },
+    movieChosen: {
+      type: String,
+      required: [true, 'Movie chosen is required.'],
+    },
+    mealChosen: {
+      type: String,
+      required: [true, 'Meal chosen is required.'],
+    },
+    // API data for meal and movie
+    movieData: {
+      type: Schema.Types.Mixed,
+      required: [true, 'Movie data from the API is required.'],
+    },
+    mealData: {
+      type: Schema.Types.Mixed,
+      required: [true, 'Meal data from the API is required.'],
+    }
   },
   {
     timestamps: true,
