8f418abd6050c72d1e03b57699f37abed3ed4919
Hamed Jalili <itjalili82@gmail.com>
2023-08-05 13:28:47 +0200

    signup update

--- login-page/src/Signup.jsx
+++ login-page/src/Signup.jsx
@@ -1,1 +1,54 @@
-<h1>Here is Signup Page</h1>
+import React, { useState } from "react";
+
+const Signup = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    console.log("Full Name:", fullName);
+    console.log("Email:", email);
+    console.log("Password:", password);
+    
+  };
+
+  return (
+    <div>
+      <h2>Signup Page</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Full Name:</label>
+          <input
+            type="text"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
