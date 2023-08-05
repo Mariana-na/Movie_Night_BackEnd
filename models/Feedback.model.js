58f494d26bf6340b336e589fad26b16623075b8b
Peter R <prodger2@gmail.com>
2023-08-05 19:34:40 +0200

    Updated the models

--- models/Feedback.model.js
+++ models/Feedback.model.js
@@ -1,1 +1,22 @@
-<h1>Here is Feedback Model Page</h1>
+const { Schema, model } = require("mongoose");
+
+const feedbackSchema = new Schema({
+  eventId: {
+    type: Schema.Types.ObjectId,
+    ref: "Event",
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+// attending/not attending will be fetched from the User model using the the userId
+
+});
+
+const Feedback = model("Feedback", feedbackSchema);
+
+module.exports = Feedback;
